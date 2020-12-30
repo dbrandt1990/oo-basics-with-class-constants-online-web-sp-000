4c47553e56d7e0872fa2aafbee181d31346db5ab
Danny Brandt <dvbrandt90@gmail.com>
2020-12-30 21:47:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,9 +14,7 @@ class Shoe
   end
   
   def brand=(brand)
-    @brand =  brand
-    if (!BRANDS.include?(brand))
-      BRANDS << brand 
-    end
+    @brand =  brand 
+    BRANDS << brand 
   end
 end
